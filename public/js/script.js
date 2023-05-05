117ec64a1439591d1dab244cb0b500129b608d27
mohatami_ <hatamitekno@gmail.com>
2023-05-05 20:09:41 +0700

    create data petugas page

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,6 @@
+// DEFINE API
+const API_URL = "https://645130aee1f6f1bb22aaa3e5.mockapi.io/api/v1/petugas";
+
 // REGISTER FUNCTIONS
 function register() {
     var username = document.getElementById("username").value;
@@ -59,3 +62,91 @@ function validate() {
     alert("Data sudah terkirim");
     return true;
 }
+
+// FORM PETUGAS FUNCTIONS
+// const form = document.getElementById("form");
+
+// form.addEventListener("submit", function(event) {
+//   event.preventDefault();
+//   const formData = new FormData(event.target);
+
+//   fetch(API_URL, {
+//     method: "POST",
+//     body: formData
+//   })
+//   .then(response => {
+//     if (response.ok) {
+//       alert("Data telah dikirimkan");
+//     } else {
+//       throw new Error("Terjadi kesalahan saat mengirim data");
+//     }
+//   })
+//   .catch(error => {
+//     console.error(error);
+//     alert("Terjadi kesalahan saat mengirim data");
+//   });
+// });
+
+function btnData() {
+    window.location.href = "./dataPetugas.html";
+}
+
+// DATA PETUGAS FUNCTIONS
+// Ambil elemen HTML untuk menampilkan data kota
+const cityContainer = document.querySelector(".city-container");
+
+// Lakukan request API untuk mendapatkan data kota
+fetch(API_URL)
+    .then((response) => response.json())
+    .then((data) => {
+        // Lakukan pengolahan data untuk menghitung jumlah data kota yang sama
+        const cityCount = data.reduce((acc, cur) => {
+            if (acc[cur.city]) {
+                acc[cur.city]++;
+            } else {
+                acc[cur.city] = 1;
+            }
+            return acc;
+        }, {});
+
+        // Tampilkan data kota dalam card
+        Object.keys(cityCount).forEach((city) => {
+            const card = `
+      <a
+  href="#"
+  class="group flex justify-between rounded-xl bg-white p-4 shadow-xl transition-shadow hover:shadow-lg sm:p-6 lg:p-8"
+>
+  <div>
+    <h3 class="text-3xl font-bold text-green-600 sm:text-5xl">${cityCount[city]}</h3>
+
+    <div class="mt-4 border-t-2 border-gray-100 pt-4">
+      <p class="text-sm font-medium uppercase text-gray-500">${city}</p>
+    </div>
+  </div>
+
+  <div
+    class="mt-8 inline-flex items-center gap-2 text-green-600 sm:mt-12 lg:mt-16"
+  >
+    <p class="font-medium sm:text-lg"></p>
+
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-6 w-6 transition-all group-hover:ms-3 rtl:rotate-180"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M17 8l4 4m0 0l-4 4m4-4H3"
+      />
+    </svg>
+  </div>
+</a>
+    `;
+            cityContainer.innerHTML += card;
+        });
+    })
+    .catch((error) => console.error(error));
